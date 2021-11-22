ff7e91e27756e5ad65a3d3d7d4623b2d7747b44e
Kumar Chitransh <kumarschitransh@gmail.com>
2021-11-22 19:43:49 +0530

    Update 1470. Shuffle the Array.java

--- 1470. Shuffle the Array.java
+++ 1470. Shuffle the Array.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int[] res = new int[2*n];
-        for(int i = 0; i < n; i++){
-          res[2 * i] = nums[i];
-          res[2 * i + 1] = nums[n+i];
+        int[] res=new int[nums.length];
+        for(int i=0;i<nums.length/2;i++){
+            res[2*i]=nums[i];
+            res[2*i+1]=nums[nums.length/2+i];
         }
         return res;
     }
